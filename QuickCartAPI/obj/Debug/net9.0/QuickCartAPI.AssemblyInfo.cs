679347f05ee2fd5de9cb457d0912903b9db8b14b
Shivam Kamboj <shivamk.step2gen@gmail.com>
2025-02-24 11:38:40 +0530

    Build changes

--- QuickCartAPI/obj/Debug/net9.0/QuickCartAPI.AssemblyInfo.cs
+++ QuickCartAPI/obj/Debug/net9.0/QuickCartAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickCartAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8bddd4cce38c5f2d3b1d874cdeb2a62e292e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a283aaaee77bf8f16d20e5b08a7da8097c7d1919")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickCartAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickCartAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
